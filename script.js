7959b60512fef543da93160f7edd7e5f1375277f
coollandmc <131864752+coollandmc@users.noreply.github.com>
2024-03-11 21:31:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var image = document.getElementById("skola_baner");
     var imageTop = image.offsetTop;
     var naslov = document.querySelector(".naslov");
-    naslov.style.top = (imageTop / 2) + imageTop + "px";
+    naslov.style.top = (imageTop / 2)  + "px";
 });
 

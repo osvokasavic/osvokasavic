bd11e17727ef47b887746820f5ba6cc091dde55a
osvokasavic <163154377+osvokasavic@users.noreply.github.com>
2024-03-12 21:00:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var image = document.getElementById("baner_skola");
-    var naslov = document.querySelector(".naslov");
+window.onload = function() {
+    // Get the banner image
+    var banner = document.querySelector('.banner');
+    // Get the naslov element
+    var naslov = document.querySelector('.naslov');
     
-    // Check if the image is loaded
-    image.onload = function() {
-        var imageHeight = image.clientHeight;
-        var imageTop = image.offsetTop;
+    // Function to calculate and set the position of the naslov element
+    function setNaslovPosition() {
+        // Get the width and height of the banner image
+        var bannerWidth = banner.offsetWidth;
+        var bannerHeight = banner.offsetHeight;
         
-        // Adjust the position of the element
-        naslov.style.top = (imageTop + imageHeight / 2) + "px";
-    };
-});
+        // Calculate the center coordinates of the banner image relative to the viewport
+        var centerX = banner.offsetLeft + bannerWidth / 2;
+        var centerY = banner.offsetTop + bannerHeight / 2;
+        
+        // Set the position of the naslov element to the calculated center coordinates
+        naslov.style.left = centerX + 'px';
+        naslov.style.top = centerY + 'px';
+    }
+    
+    // Call the setNaslovPosition function when the window is resized (in case the image size changes) and initially
+    window.addEventListener('resize', setNaslovPosition);
+    setNaslovPosition(); // Call initially
+};

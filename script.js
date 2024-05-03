ceb382aebe536507f2e5d0ae2aaf0dab70f65e00
osvokasavic <163154377+osvokasavic@users.noreply.github.com>
2024-05-03 09:56:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -67,13 +67,6 @@ function closeDropdown(dropdownId) {
     dropdown.style.display = "none";
 }
 
-document.addEventListener('click', function(event) {
-    // Check if the clicked element has the class "overlay"
-    if (!event.target.closest('.overlay-content')) {
-        // If it doesn't have the class, execute closeDropdown function
-        closeDropdown([1, 2, 3]); // Pass an array of dropdown IDs
-    }
-});
 function toggleDropdown(dropdownId) {
     var dropdown = document.getElementById("dropdown" + dropdownId);
     if (dropdown.style.display === "block") {
